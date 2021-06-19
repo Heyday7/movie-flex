093219c481ea9171787324cb67925f68e5361a3b
정인영 <jbjb4790@gmail.com>
2021-06-20 06:12:10 +0900

    감독 + 영화 데이터 받아오기 감독 데이터가 없는 영화 핸들링 필요함

--- src/components/quiz/directorQuiz/index.js
+++ src/components/quiz/directorQuiz/index.js
@@ -1,15 +1,62 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { movieApi } from '../../../api/movieApi';
 import './directorQuiz.css';
 
 function DirectorQuiz() {
+  const [movies, setMovies] = useState(null);
+  const [dirMovies, setDirMovies] = useState(null);
+  const [director, setDirector] = useState(null);
+  const getFormatDate = (date) => {
+    const year = date.getFullYear();
+    let month = (1 + date.getMonth());
+    month = month >= 10 ? month : `0${month}`;
+    let day = date.getDate();
+    day = day >= 10 ? day : `0${day}`;
+    return `${year}${month}${day}`;
+  };
+  const randomDate = (start, end) => {
+    const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    return getFormatDate(date);
+  };
   useEffect(() => {
     const getData = async () => {
-      const { data } = await movieApi.boxOfficeData(20210612);
-      console.log(data);
+      const randDate = randomDate(new Date(2010, 0, 1), new Date());
+      // eslint-disable-next-line no-return-await
+      return await movieApi.boxOfficeData(randDate);
     };
-    getData();
+    getData()
+      .then((res) => {
+        console.log(res.data.boxOfficeResult.dailyBoxOfficeList[0].movieNm);
+        setMovies(res.data.boxOfficeResult.dailyBoxOfficeList.slice(1, 5));
+        return res.data.boxOfficeResult.dailyBoxOfficeList[0].movieNm;
+      })
+      .then((tmpMovie) => {
+        console.log(tmpMovie);
+        const getDirector = async (tmpMovie) => {
+          const { data } = await movieApi.movieListByName(tmpMovie);
+          console.log('data!!');
+          console.log(data);
+          return data.movieListResult.movieList[0].directors[0].peopleNm;
+        };
+        getDirector(tmpMovie)
+          .then((dir) => {
+            setDirector(dir);
+            const getDirMovies = async (dir) => {
+              const { data } = await movieApi.movieListByDirector(dir);
+              console.log('data!');
+              console.log(data);
+              return data.movieListResult.movieList;
+            };
+            getDirMovies(dir)
+              .then((dirMovieList) => {
+                setDirMovies(dirMovieList);
+                console.log(dirMovieList);
+              });
+            return dir;
+          });
+      });
   }, []);
+  console.log(randomDate(new Date(2010, 0, 1), new Date()));
   return (
     <div>
       <header>
@@ -26,8 +73,22 @@ function DirectorQuiz() {
       <body>
         <div className="quiz4">4. 감독으로 영화 맞추기</div>
         <div className="question4">다음 영화감독이 만든 작품을 모두 골라보세요</div>
-        <div className="director_box">감독이름</div>
-        <div className="answer_box">영화이름</div>
+        <div className="director_box">
+          {director}
+        </div>
+        { dirMovies && (
+          <div>
+            <div>
+              대표작:
+              {dirMovies[0].movieNm}
+            </div>
+            <div className="answer_sheet">
+              {dirMovies.map((dirmovie) =>
+                <button className="answer_box">{dirmovie.movieNm}</button>
+              )}
+            </div>
+          </div>
+        )}
         <div>
           <button className="next_button">다음 퀴즈</button>
         </div>
